d942d4ce634d14c1920e646dfc66cffb6113825e
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-11-15 00:00:56 +1000

    create client component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { ClientsComponent } from './components/clients/clients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,

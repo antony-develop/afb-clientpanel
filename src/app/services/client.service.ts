bbbace88089fc0afd26c4ce0bfffd984f5d62b5f
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-11-14 23:56:28 +1000

    create client serice

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from "rxjs";
+import { map } from 'rxjs/operators';
+import { Client } from "../models/Client";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  clientsCollection: AngularFirestoreCollection<Client>;
+  clientDoc: AngularFirestoreDocument<Client>;
+  clients: Observable<Client[]>
+  client: Observable<Client>
+
+  constructor(private angularFireStore: AngularFirestore) {
+    this.clientsCollection = this.angularFireStore.collection('clients', ref => {
+      return ref.orderBy('lastName');
+    })
+   }
+
+   getClients(): Observable<Client[]> {
+    this.clients = this.clientsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Client;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+
+    return this.clients;
+   }
+}

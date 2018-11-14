2a43900cb8b50959099525ef8ef2865fdeffb3f7
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-11-14 22:51:38 +1000

    create firebase object in envirement var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA75d_Zdndk717fjki2YNajEAkKKKzrT8M",
+    authDomain: "afb-clientpanel.firebaseapp.com",
+    databaseURL: "https://afb-clientpanel.firebaseio.com",
+    projectId: "afb-clientpanel",
+    storageBucket: "afb-clientpanel.appspot.com",
+    messagingSenderId: "541103576759"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA75d_Zdndk717fjki2YNajEAkKKKzrT8M",
+    authDomain: "afb-clientpanel.firebaseapp.com",
+    databaseURL: "https://afb-clientpanel.firebaseio.com",
+    projectId: "afb-clientpanel",
+    storageBucket: "afb-clientpanel.appspot.com",
+    messagingSenderId: "541103576759"
+  }
 };
 
 /*

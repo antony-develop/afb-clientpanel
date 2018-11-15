0c4835df15edd9a0b25cd70bb21a4efae85d90ab
Anton Kulaga <19634898+antony-develop@users.noreply.github.com>
2018-11-15 21:33:33 +1000

    add rout for add-client component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'client/add', component: AddClientComponent },
   { path: '**', component: NotFoundComponent },
 ];
 
